6ff9427f0c6353778b8dc23d8cd1afbe920c33f7
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-08-16 12:14:10 +0200

    add clipboard copy button, remove existing copy button, add clear button, add emoji array

--- src/App.js
+++ src/App.js
@@ -1,36 +1,49 @@
 import React, { Component } from 'react';
+import ClipboardButton from 'react-clipboard.js';
 import Input from './Components/Input';
 import Output from './Components/Output';
-import Button from './Components/Button';
+import ClearButton from './Components/ClearButton';
 import Count from './Components/Count';
 import logo from './logo.svg';
 import './App.css';
+
+const emojis = [0x1f499, 0x1f49a, 0x1f49b, 0x1f49c, 0x1f495];
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       value: '',
-      count: 0
+      count: 0,
+      emoji: emojis[0]
     };
   }
   onInputChange(value) {
-    let sign = String.fromCodePoint(0x1f49c);
+    let sign = String.fromCodePoint(this.state.emoji);
     let newValue = value.split(' ').join(` ${sign} `);
     this.setState({
       value: newValue,
       count: newValue.length
     });
   }
-
+  onClearClick() {
+    this.setState({
+      value: '',
+      count: 0
+    });
+  }
   render() {
     return (
       <div>
         <Input onInputChange={value => this.onInputChange(value)} />
         <Output value={this.state.value} />
         <Count count={this.state.count} />
-        <Button action="copy" />
-        <Button action="clear" />
+        <ClipboardButton
+          data-clipboard-text={this.state.value}
+          button-title="Copy to clipboard">
+          Copy
+        </ClipboardButton>
+        <ClearButton onClearClick={() => this.onClearClick()} />
       </div>
     );
   }
